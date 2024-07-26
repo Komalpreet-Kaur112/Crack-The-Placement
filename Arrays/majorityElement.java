49d878c80e1a4354b5fd674b5a501f402ddb66b0
Komalpreet-Kaur112 <143227316+Komalpreet-Kaur112@users.noreply.github.com>
2024-07-26 23:20:22 +0530

    majorityElement created

--- Arrays/majorityElement.java
+++ Arrays/majorityElement.java
@@ -1,5 +1,31 @@
 package Arrays;
 
 public class majorityElement {
-    
+    public int majorityElement(int[] nums) {
+    int n = nums.length;
+        int count = 0;
+        int el = 0;
+        for(int i = 0;i<n;i++){
+            if(count == 0){
+                count++;
+                 el = nums[i];
+            }
+            else if(nums[i] == el){
+                count++;
+            }
+            else{
+                count--;
+            }
+        }
+        int cnt = 0;
+        for(int i = 0;i<n;i++){
+            if(nums[i] == el){
+                cnt++;
+            }
+        }
+        if(cnt > (n/2)){
+            return el;
+        }
+        return -1;
+    }
 }
